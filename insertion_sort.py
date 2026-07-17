8df9b247d520f22b5e23cb052938fe493248b9bc
nambiarlakshmi <nambiarlakshmi@outlook.com>
2026-07-17 12:17:44 -0500

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(a):
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > key:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+    return a
+
+a = [4, 8, 1, 3, 5, 2]
+print(insertion_sort(a))
