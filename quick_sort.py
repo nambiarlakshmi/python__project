cd8aab3e09ec9b65791a6cb8b0f81a9926e0b560
nambiarlakshmi <nambiarlakshmi@outlook.com>
2026-07-20 16:54:28 -0500

    Add files via upload

---
+++ quick_sort.py
@@ -0,0 +1,11 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[-1]
+    left = [x for x in arr[:-1] if x <= pivot]
+    right = [x for x in arr[:-1] if x > pivot]
+    return quick_sort(left) + [pivot] + quick_sort(right)
+arr = [8, 3, 1, 7, 0, 10, 2]
+print(quick_sort(arr))
+
+# I used the sudocode in the notes to make my code
